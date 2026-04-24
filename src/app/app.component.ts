258ffff48051531715963544e4f7c1b896a6bb06
Nihanth Gandhi <gandhinihanth@gmail.com>
2026-04-24 22:10:00 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,20 @@ import { FooterComponent } from './core/layout/footer.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1 style="text-align:center; color:red;">
+      {{ buildVersion }}
+    </h1>
+
+    <app-header></app-header>
+
+    <router-outlet></router-outlet>
+
+    <app-footer></app-footer>
+  `,
   imports: [HeaderComponent, RouterOutlet, FooterComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  buildVersion = 'BUILD VERSION 1';
+}
