e5d253cb9e3497775f752354679e53564fc4144a
Nihanth Gandhi <gandhinihanth@gmail.com>
2026-04-24 21:05:33 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+console.log('Build time:', new Date().toISOString());// 👈 change this value each time
+
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
